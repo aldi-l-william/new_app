e4ad6287bf2996823e74a5bb1d15c39bf101c596
Aldi William <aldilwilliam2023@gmail.com>
2024-12-19 12:24:25 +0700

    new installation

---
+++ components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="max-1200-container flex justify-between">
+        <div>
+            <img src="https://project-exterior-technical-test-app.up.railway.app/img/logo.png" alt="Wisata App" class="w-[200px]"/>
+        </div>
+        <div>
+            <button class="px-4 py-2">
+                Ini
+            </button>
+        </div>
+    </div>
+</template>
